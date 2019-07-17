b990c0d69f739052a1a4e080f13e8880820520cf
Phil Booth <pmbooth@gmail.com>
2019-07-17 16:15:50 +0100

    fix(package): allow ../ references to fxa-shared and fxa-js-client

--- packages/fxa-content-server/webpack.config.js
+++ packages/fxa-content-server/webpack.config.js
@@ -27,6 +27,7 @@ const webpackConfig = {
       'js-md5',
       'modal',
       'raven',
+      'sjcl',
       'speed-trap',
       'ua-parser-js',
       'uuid',
@@ -46,7 +47,7 @@ const webpackConfig = {
   },
 
   resolve: {
-    extensions: ['.ts', '.js','.jsx'],
+    extensions: ['.ts', '.js', '.jsx'],
     modules: [
       path.resolve(__dirname, 'app/scripts'),
       path.resolve(__dirname, 'app/scripts/templates'),
@@ -78,7 +79,6 @@ const webpackConfig = {
         __dirname,
         'node_modules/es6-promise/dist/es6-promise'
       ),
-      fxaClient: 'fxa-js-client/client/FxAccountClient',
       fxaCryptoDeriver: path.resolve(
         __dirname,
         'node_modules/fxa-crypto-relier/dist/fxa-crypto-relier/fxa-crypto-deriver'
@@ -175,17 +175,17 @@ const webpackConfig = {
             options: {
               cacheDirectory: true,
               presets: [
-                [
-                  '@babel/preset-react', {
-                  }
-                ],
-                '@babel/preset-env', 
-                '@babel/preset-typescript'
+                ['@babel/preset-react', {}],
+                '@babel/preset-env',
+                '@babel/preset-typescript',
               ],
-              plugins: ['@babel/syntax-dynamic-import', '@babel/plugin-proposal-class-properties']
-            }
-          }
-        ]
+              plugins: [
+                '@babel/syntax-dynamic-import',
+                '@babel/plugin-proposal-class-properties',
+              ],
+            },
+          },
+        ],
       },
       {
         test: /\.scss$/,
